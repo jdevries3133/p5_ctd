4a2da0955f5368f09d4072559ab6c0ef4216861b
Jack DeVries <jdevries3133@gmail.com>
2023-11-13 21:17:11 -0500

    doc: add comments to `sketch.js`

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ function setup() {
   // If you want your drawing to take up a larger part of the screen, you can
   // just make this bigger!
   createCanvas(400, 400);
+
+  // This sets the background for the snowman. You might want to move it
   background(184, 224, 255);
 }
 
@@ -10,6 +12,10 @@ function setup() {
 // see any changes.
 
 function draw() {
+  // Unlike Khan Academy, though, you need to put all your drawing code inside
+  // the `draw` function here. You can still set variables at the top-level,
+  // and then modify them inside the draw function to do animations. You just
+  // can't call drawing functions like `ellipse` outside the `draw` function.
   fill(0, 100, 255);
   ellipse(100, 60, 40, 40);
   ellipse(100, 115, 70, 70);
